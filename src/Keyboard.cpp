c817ad1fe28f5be445efbee1019209c0fc43da06
Mark McKinney <markp.mckinney@gmail.com>
2017-01-31 13:54:26 -0800

    Added M key toggle for showing/hiding mouse

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -9,6 +9,7 @@
 #include "Keyboard.hpp"
 
 bool Keyboard::keyToggles[349] = {false};
+static bool hideCursor = true;
 
 // This function is called when a key is pressed
 void Keyboard::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
@@ -17,6 +18,15 @@ void Keyboard::key_callback(GLFWwindow *window, int key, int scancode, int actio
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
     
+    if (key == GLFW_KEY_M && action == GLFW_PRESS) {
+        hideCursor = !hideCursor;
+        if (hideCursor) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        } else {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+    }
+
     if (action == GLFW_PRESS) {
         keyToggles[key] = true;
     }
